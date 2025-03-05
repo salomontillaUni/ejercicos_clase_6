0ac526a5721d299be24b87bb6dadc512ed01d469
salo <salomontilla@outlook.com>
2025-03-05 15:55:00 -0500

    corrige ejercicio 6

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -7,17 +7,17 @@ public class Ejercicio6 {
         int base = sc.nextInt();
         System.out.println("Ingrese el exponente de la potencia: ");
         int exp = sc.nextInt();
-        int j = 0;
         double resultado = 1;
         if(exp == 0){
             System.out.println("El resultado de la potencia es: " + 1);
         }else if(exp < 0){
-            int expNeg = 1/-exp;
-            resultado = Math.pow(base, expNeg);
+            exp = exp * -1;
+            resultado = 1 / (Math.pow(base, exp));
+            System.out.println("El resultado de la potencia es: " + resultado);
         }else{
             resultado = Math.pow(base, exp);
+            System.out.println("El resultado de la potencia es: " + resultado);
         }
 
-        System.out.println("El resultado de la potencia es: " + resultado);
     }
 }
