0e816c31df28e44d69d34bff463f15ea4e94dda8
Salomon <salomon.montilla.l@gmail.com>
2025-03-05 08:47:18 -0500

    termina ejercicio 6

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la base de la potencia: ");
+        int base = sc.nextInt();
+        System.out.println("Ingrese el exponente de la potencia: ");
+        int exp = sc.nextInt();
+        int j = 0;
+        double resultado = 1;
+        if(exp == 0){
+            System.out.println("El resultado de la potencia es: " + 1);
+        }else if(exp < 0){
+            int expNeg = 1/-exp;
+            resultado = Math.pow(base, expNeg);
+        }else{
+            resultado = Math.pow(base, exp);
+        }
 
+        System.out.println("El resultado de la potencia es: " + resultado);
     }
 }
